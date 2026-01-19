b77a3cd2c471931b284b1a19f767c17979f47722
julianu <julian.uszkoreit@rub.de>
2026-01-19 14:07:30 +0000

    use PSI accessions for feature mzQC

--- bin/extract_from_featurexml.py
+++ bin/extract_from_featurexml.py
@@ -3,67 +3,22 @@
 import os
 import argparse
 import base64
-from typing import Any, List
 import zipfile
 import pyopenms
-from collections import defaultdict
 import h5py
-import numpy as np
+from collections import defaultdict
+
+import hdf5_functions as mzhdf5
+
 
 def argparse_setup():
     parser = argparse.ArgumentParser()
     parser.add_argument("-featurexml", help="FeatureXML with already annotated identifications")
     parser.add_argument("-out_hdf5", help="The Output statistics HDF5")
-    parser.add_argument("-report_up_to_charge", default=5, help="Upper limit of range to be reported in a csv table for the charge")
+    parser.add_argument("-report_up_to_charge", type=int, default=5, help="Upper limit of range to be reported in a csv table for the charge")
 
     return parser.parse_args()
 
-
-def add_entry_to_hdf5(
-    f, qc_acc: str, qc_short_name: str, qc_name: str, qc_description: str, 
-    value, value_shape: tuple, value_type: str, 
-    unit_accession: str=None, unit_name: str=None,
-    ): 
-    """ Adds an entry into the hdf5 file """
-    key = "|".join([qc_acc, qc_short_name])  # ACCESSION|SHORT_DESC
-    if value_type in ("str", h5py.string_dtype()):
-        ds = f.create_dataset(key, shape=value_shape, dtype=h5py.string_dtype(), compression="gzip")
-        ds[:] = value
-    else:
-        f.create_dataset(key, value_shape, dtype=value_type, compression="gzip")
-        if not any([x == 0 for x in value_shape]):
-            # Check if any dimension is 0. If so, we do not write data in it (zero lengthed result).
-            f[key].write_direct(np.array(value, dtype=value_type))
-    
-    f[key].attrs["qc_short_name"] = qc_short_name
-    f[key].attrs["qc_name"] = qc_name
-    f[key].attrs["qc_description"] = qc_description
-    f[key].attrs["unit_accession"] = unit_accession
-    f[key].attrs["unit_name"] = unit_name
-
-
-
-def add_table_in_hdf5(
-    f, qc_acc: str, qc_short_name: str, qc_name: str, qc_description: str, 
-    column_names: List[str], column_data: List[List[Any]], column_types: List[str]
-    ): 
-    """Adds a table in groups"""
-
-    key = "|".join([qc_acc, qc_short_name])  # ACCESSION|SHORT_DESC
-    table_group = f.create_group(key)
-    table_group.attrs["qc_short_name"] = qc_short_name
-    table_group.attrs["qc_name"] = qc_name
-    table_group.attrs["qc_description"] = qc_description
-    table_group.attrs["column_order"] = "|".join(column_names)
-    
-    for n, d, t in zip (column_names, column_data, column_types): 
-        if t in ("str", h5py.string_dtype()):
-            ds = table_group.create_dataset(n, shape=len(d), dtype=h5py.string_dtype(), compression="gzip")
-            ds[:] = d
-        else:
-            table_group.create_dataset(n, (len(d),), dtype=t, compression="gzip")
-            table_group[n].write_direct(np.array(d, dtype=t))
-    
 
 if __name__ == "__main__":
     args = argparse_setup()
@@ -77,7 +32,6 @@ if __name__ == "__main__":
     total_num_ident_features = 0
     num_ident_features_charge = defaultdict(lambda: 0) # Identified by charge state
     for f in features:
-
         # Get data to be able to count
         charge = f.getCharge()
         idents_of_f = f.getPeptideIdentifications()
@@ -97,60 +51,95 @@ if __name__ == "__main__":
     with open("featurexml.zip", "rb") as fb:
         feature_str_bs64 = base64.b64encode(fb.read())
 
-    with h5py.File(args.out_hdf5, 'w') as out_h5:
-        add_entry_to_hdf5(
-            out_h5,
-            "MS:4000102",
-            "nr_features",
-            "number of detected quantification data points",
-            (
+    with h5py.File(args.out_hdf5, "w") as out_h5:
+        mzhdf5.add_entry_to_hdf5(
+            f=out_h5,
+            qc_acc="MS:4000102",
+            qc_short_name="nr_features",
+            qc_name="number of detected quantification data points",
+            qc_description=(
                 "The number of data points detected for quantification purposes within the run. "
                 "These data points may be for example XIC profiles, isotopic pattern areas, or reporter ions (see MS:1001805). "
-                "The used type should be noted in the metadata or analysis methods section of the recording file for the respective run.",
+                "The used type should be noted in the metadata or analysis methods section of the recording file for the respective run."
             ),
-            total_num_features,
-            (1,),
-            "int64",
+            value=total_num_features,
+            value_shape=(1,),
+            value_type="uint64",
             unit_accession="UO:0000189",
-            unit_name="count int"
+            unit_name="count unit",
         )
 
-        add_entry_to_hdf5(
-            out_h5,
-            "MS:4000103",
-            "nr_ident_features",
-            "number of identified quantification data points",
-            (
+        mzhdf5.add_entry_to_hdf5(
+            f=out_h5,
+            qc_acc="MS:4000103",
+            qc_short_name="nr_ident_features",
+            qc_name="number of identified quantification data points",
+            qc_description=(
                 "The number of identified data points for quantification purposes within the run after user defined acceptance criteria are applied. "
                 "These data points may be for example XIC profiles, isotopic pattern areas, or reporter ions (see MS:1001805). "
                 "The used type should be noted in the metadata or analysis methods section of the recording file for the respective run. "
-                "In case of multiple acceptance criteria (FDR) available in proteomics, PSM-level FDR should be used for better comparability.",
+                "In case of multiple acceptance criteria (FDR) available in proteomics, PSM-level FDR should be used for better comparability."
             ),
-            total_num_ident_features,
-            (1,),
-            "int64",
+            value=total_num_ident_features,
+            value_shape=(1,),
+            value_type="uint64",
             unit_accession="UO:0000189",
-            unit_name="count int"
+            unit_name="count unit",
         )
 
-        add_table_in_hdf5(
-            out_h5,
-            "LOCAL:06",
-            "features_charge",
-            "Number of features with charge",
-            "Number of features with charge",
-            [str(i) for i in range(1, int(args.report_up_to_charge) + 1)],
-            [[num_features_charge[i]] for i in range(1, int(args.report_up_to_charge) + 1)],
-            ["int64" for i in range(1, int(args.report_up_to_charge) + 1)]
+        report_up_to_charge = args.report_up_to_charge
+        charges_features_more = 0
+        for key in num_features_charge.keys():
+            if key > report_up_to_charge:
+                charges_features_more += num_features_charge[key]
+
+        charges_features_frac_list = [
+            num_features_charge[i] / total_num_features
+            for i in range(1, report_up_to_charge + 1)
+        ] + [charges_features_more / total_num_features]
+
+        mzhdf5.add_table_to_hdf5(
+            f=out_h5,
+            qc_acc="MS:4000207",
+            qc_short_name="features_charges",
+            qc_name="detected quantification data points charges fractions",
+            qc_description=(
+                "The fraction of all data points detected for quantification purposes within the run for each specified charge state. "
+                "The fractions [0,1] are given in the 'fraction' column, corresponding charges in the 'charge state' column. "
+                "The highest charge state is to be interpreted as that charge state or higher."
+            ),
+            column_names=["MS:1000041 ! charge state", "UO:0000191 ! fraction"],
+            column_data=[
+                [int(i) for i in range(1, int(report_up_to_charge) + 2)],
+                charges_features_frac_list,
+            ],
+            column_types=["uint64", "float64"],
         )
 
-        add_table_in_hdf5(
-            out_h5,
-            "LOCAL:07",
-            "ident_features_charge",
-            "Number of identified features with charge",
-            "Number of identified features with charge",
-            [str(i) for i in range(1, int(args.report_up_to_charge) + 1)],
-            [[num_ident_features_charge[i]] for i in range(1, int(args.report_up_to_charge) + 1)],
-            ["int64" for i in range(1, int(args.report_up_to_charge) + 1)]
+        charges_features_id_more = 0
+        for key in num_ident_features_charge.keys():
+            if key > report_up_to_charge:
+                charges_features_id_more += num_ident_features_charge[key]
+
+        charges_features_id_frac_list = [
+            num_ident_features_charge[i] / total_num_ident_features
+            for i in range(1, report_up_to_charge + 1)
+        ] + [charges_features_id_more / total_num_ident_features]
+
+        mzhdf5.add_table_to_hdf5(
+            f=out_h5,
+            qc_acc="MS:4000208",
+            qc_short_name="ident_features_charge",
+            qc_name="identified quantification data points charges fractions",
+            qc_description=(
+                "The fraction of all data points detected and identified for quantification purposes within the run for each specified charge state. "
+                "The fractions [0,1] are given in the 'fraction' column, corresponding charges in the 'charge state' column. "
+                "The highest charge state is to be interpreted as that charge state or higher."
+            ),
+            column_names=["MS:1000041 ! charge state", "UO:0000191 ! fraction"],
+            column_data=[
+                [int(i) for i in range(1, int(report_up_to_charge) + 2)],
+                charges_features_id_frac_list,
+            ],
+            column_types=["uint64", "float64"],
         )
