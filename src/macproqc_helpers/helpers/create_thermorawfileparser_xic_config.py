cc0ad3047895565e96d62afedd51c1987c411278
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2026-06-26 08:56:20 +0000

    make linter and formatter happy

--- src/macproqc_helpers/helpers/create_thermorawfileparser_xic_config.py
+++ src/macproqc_helpers/helpers/create_thermorawfileparser_xic_config.py
@@ -4,10 +4,14 @@ import csv
 import json
 import argparse
 from statistics import mean
+from typing import Any, Dict, List
 from pyteomics import proforma
 
 
 def argparse_setup(subparsers: argparse._SubParsersAction):
+    """
+    Set up the argument parser for the create_thermorawfileparser_xic_config command.
+    """
     parser = subparsers.add_parser(
         "create_thermorawfileparser_xic_config",
         help="Create a JSON-File to be used with the ThermoRawFileParser XIC and alphatims",
@@ -19,7 +23,8 @@ def argparse_setup(subparsers: argparse._SubParsersAction):
         help="Input-Identification-Files in mzTAB format (here the SpikeIns are specially encoded (see FASTA))",
     )
     parser.add_argument(
-        "-ojson", help="The JSON-Output-File to be used with the ThermoRawFileParser XIC"
+        "-ojson",
+        help="The JSON-Output-File to be used with the ThermoRawFileParser XIC",
     )
     parser.add_argument(
         "-oidentifications",
@@ -29,9 +34,13 @@ def argparse_setup(subparsers: argparse._SubParsersAction):
 
 
 def create(args: argparse.Namespace):
-    seq_to_data = dict()
+    """
+    Create a JSON-File to be used with the ThermoRawFileParser XIC and alphatims
+    """
 
-    with open(args.icsv, "r") as in_csv_file:
+    seq_to_data: Dict[str, Dict[str, Any]] = dict()
+
+    with open(args.icsv, "r", encoding="utf-8") as in_csv_file:
         # Read spike-ins CSV and get header indicies
         csv_in = csv.reader(in_csv_file)
         header = next(csv_in)
@@ -43,56 +52,54 @@ def create(args: argparse.Namespace):
         rt_tol_idx = header.index("rt-tol")
 
         # Generate the two output structures
-        for l in csv_in:
+        for row in csv_in:
             # go line-wise through the spike-ins CSV
 
-            mz_tol_split = str(l[mz_tol_idx]).split(sep=" ")
-            rt_tol = float(l[rt_tol_idx]) / 60
+            mz_tol_split = str(row[mz_tol_idx]).split(sep=" ")
+            rt_tol = float(row[rt_tol_idx]) / 60
 
             # transform the proforma sequence to a plain sequence (only amino acids, no modifications) to be able to check for identifications, which are only annotated with plain sequences
-            proforma_sequence = proforma.parse(str(l[seq_idx]))
+            proforma_sequence = proforma.parse(str(row[seq_idx]))
             plain_sequence = "".join(aa for aa, mods in proforma_sequence[0])
 
             # map from the sequence to all other data (basically our json)
             seq_to_data[plain_sequence] = {
-                "comment": str(l[name_idx]),
-                "mz": float(l[mz_idx]),
+                "comment": str(row[name_idx]),
+                "mz": float(row[mz_idx]),
                 "tolerance": float(mz_tol_split[0]),
                 "tolerance_unit": str(mz_tol_split[1]),
-                "rt_start": (float(l[rt_idx]) / 60) - rt_tol,
-                "rt_end": (float(l[rt_idx]) / 60) + rt_tol,
+                "rt_start": (float(row[rt_idx]) / 60) - rt_tol,
+                "rt_end": (float(row[rt_idx]) / 60) + rt_tol,
             }
 
-    found_rts = dict()
-    with open(args.iidents, "r") as in_ident_file:
+    found_rts: Dict[str, List[float]] = dict()
+    with open(args.iidents, "r", encoding="utf-8") as in_ident_file:
         rt_idx = 0
-        accession_idx = 0
         score_label = ""
         score_idx = 0
-        for l in in_ident_file:
+        for line in in_ident_file:
             # iterate through mzTab
 
             # Get Header Line
-            if l.startswith("MTD"):
-                cols = l.split("\t")
+            if line.startswith("MTD"):
+                cols = line.split("\t")
                 if cols[1].startswith("psm_search_engine_score") and ("MS:1002257" in cols[2]):
                     # this is the "comet expression" score, remove the "psm_" prefix
                     score_label = cols[1][4:]
-            elif l.startswith("PSH"):
-                cols = l.split("\t")
+            elif line.startswith("PSH"):
+                cols = line.split("\t")
                 seq_idx = cols.index("sequence")
                 rt_idx = cols.index("retention_time")
-                accession_idx = cols.index("accession")
                 if len(score_label) > 0:
                     score_idx = cols.index(score_label)
 
             # Check if we found an psms which actually fits to the SpikeIns and put it into the found RTs
-            if l.startswith("PSM"):
-                cols = l.split("\t")
-                if cols[seq_idx] in seq_to_data.keys():
+            if line.startswith("PSM"):
+                cols = line.split("\t")
+                if cols[seq_idx] in seq_to_data:
                     if score_idx < 1 or float(cols[score_idx]) < 0.01:
                         # either cannot filter for score or it is below 0.01
-                        if cols[seq_idx] not in found_rts.keys():
+                        if cols[seq_idx] not in found_rts:
                             found_rts[cols[seq_idx]] = []
 
                         found_rts[cols[seq_idx]].append(float(cols[rt_idx]))
@@ -106,15 +113,15 @@ def create(args: argparse.Namespace):
             seq_to_data[seq]["rt_start"] = new_rt - tol
             seq_to_data[seq]["rt_end"] = new_rt + tol
 
-    for seq in seq_to_data.keys():
-        seq_to_data[seq]["rt_start"] = max(0, seq_to_data[seq]["rt_start"])
+    for _seq, data in seq_to_data.items():
+        data["rt_start"] = max(0, data["rt_start"])
 
     # Write Output JSON-File
-    with open(args.ojson, "w") as o_json:
+    with open(args.ojson, "w", encoding="utf-8") as o_json:
         o_json.write(json.dumps([val for val in seq_to_data.values()], indent=4))
 
     # Write Output identifications file
-    with open(args.oidentifications, "w") as output_identifications:
+    with open(args.oidentifications, "w", encoding="utf-8") as output_identifications:
         for seq, vals in found_rts.items():
             nr_ids = len(set(vals))
             output_identifications.write(seq + "," + str(nr_ids) + "\n")
